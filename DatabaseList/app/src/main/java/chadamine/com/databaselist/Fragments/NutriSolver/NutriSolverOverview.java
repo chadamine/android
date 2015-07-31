3e9493cc0c45ee4743e33d0f8a9445715279de76
chadamine <chadwicklew@gmail.com>
2015-07-31 06:27:26 -0600

    NutriSolver setup

---
+++ DatabaseList/app/src/main/java/chadamine/com/databaselist/Fragments/NutriSolver/NutriSolverOverview.java
@@ -0,0 +1,36 @@
+package chadamine.com.databaselist.Fragments.NutriSolver;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import chadamine.com.databaselist.R;
+
+public class NutriSolverOverview extends Fragment {
+
+    public static NutriSolverOverview newInstance(Bundle args) {
+        NutriSolverOverview f = new NutriSolverOverview();
+        f.setArguments(args);
+        return f;
+    }
+
+    public NutriSolverOverview() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_nutrisolver_overview, container, false);
+    }
+
+
+}
