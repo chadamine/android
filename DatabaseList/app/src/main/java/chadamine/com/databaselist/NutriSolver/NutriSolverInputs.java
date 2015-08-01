97ba954c9a972979fb68b963be132a24898414e5
chadamine <chadwicklew@gmail.com>
2015-08-01 06:30:25 -0600

    nutrisolver menu

--- DatabaseList/app/src/main/java/chadamine/com/databaselist/NutriSolver/NutriSolverInputs.java
+++ DatabaseList/app/src/main/java/chadamine/com/databaselist/NutriSolver/NutriSolverInputs.java
@@ -1,20 +1,33 @@
 package chadamine.com.databaselist.NutriSolver;
 
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import chadamine.com.databaselist.Adapters.ListCursorAdapter;
+import chadamine.com.databaselist.Objects.Nutrient;
 import chadamine.com.databaselist.R;
 
-public class NutriSolverInputs extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class NutriSolverInputs extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private String mParam1;
-    private String mParam2;
+    private Context mContext;
+    private Nutrient mNutrient;
+    private Bundle mBundle;
+    private ListCursorAdapter mListCursorAdapter;
+
+    private static final int LIST_LOADER_ID = 0;
 
     public static NutriSolverInputs newInstance(Bundle args) {
         NutriSolverInputs f = new NutriSolverInputs();
@@ -22,21 +35,66 @@ public class NutriSolverInputs extends Fragment {
         return f;
     }
 
-    public NutriSolverInputs() {
-        // Required empty public constructor
+    public NutriSolverInputs() {}
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ListView list = getListView();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //if (getArguments() != null) {}
+
+        setRetainInstance(true);
+        setHasOptionsMenu(true);
+
+        mContext = getActivity();
+
+        if (getArguments() != null) {
+            mBundle = getArguments();
+        }
 
 
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nutrisolver_inputs, container, false);
+        View view = inflater.inflate(R.layout.fragment_nutrisolver_inputs, container, false);
+
+        return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_nutrisolver_add_nutrient, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(mContext, mNutrient.getUri(),
+                mNutrient.getKeyIdArray(), null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mListCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mListCursorAdapter.swapCursor(null);
+    }
+
 }
