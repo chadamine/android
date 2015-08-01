dca458654230f9d0247e2ef8a80a41a14ba17f46
chadamine <chadwicklew@gmail.com>
2015-08-01 03:29:00 -0600

    tabs, pager, adapter

--- DatabaseList/app/src/main/java/chadamine/com/databaselist/NutriSolver/NutriSolver/NutriSolverOverview.java
+++ DatabaseList/app/src/main/java/chadamine/com/databaselist/NutriSolver/NutriSolver/NutriSolverOverview.java
@@ -1,15 +1,21 @@
 package chadamine.com.databaselist.NutriSolver.NutriSolver;
 
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.astuetz.PagerSlidingTabStrip;
+
+import chadamine.com.databaselist.Adapters.CustomFragmentPagerAdapter;
 import chadamine.com.databaselist.R;
 
 public class NutriSolverOverview extends Fragment {
+
+    Bundle mBundle;
 
     public static NutriSolverOverview newInstance(Bundle args) {
         NutriSolverOverview f = new NutriSolverOverview();
@@ -22,15 +28,25 @@ public class NutriSolverOverview extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+
         if (getArguments() != null) {
+            mBundle = getArguments();
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nutrisolver_overview, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_nutrisolver_overview, container, false);
+        CustomFragmentPagerAdapter adapter = new CustomFragmentPagerAdapter(getFragmentManager(), getActivity(), mBundle);
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) view.findViewById(R.id.pager_sliding_tab_strip);
+        ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+        tabs.setViewPager(pager);
+
+        return view;
     }
-
-
 }
